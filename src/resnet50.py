d157edde89a92b8382745a29acd57ae322156cbb
Ronja <ronjag@elektro.com>
2020-12-10 13:35:23 +0100

    resnet34, resnet18

--- src/resnet50.py
+++ src/resnet50.py
@@ -336,18 +336,20 @@ class MultiPrototypes(nn.Module):
             out.append(getattr(self, "prototypes" + str(i))(x))
         return out
 
+def resnet18(**kwargs):
+    return ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+
+def resnet34(**kwargs):
+    return ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
 
 def resnet50(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
 
-
 def resnet50w2(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], widen=2, **kwargs)
 
-
 def resnet50w4(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], widen=4, **kwargs)
-
 
 def resnet50w5(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], widen=5, **kwargs)
